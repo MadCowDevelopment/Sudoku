6f5cd12206fb4d73b2ab44ed5655f4f3cde1283b
Mihi <devdumbblind@googlemail.com>
2012-02-18 16:56:36 +0100

    added stub for executable tools

---
+++ Sudoku.ViewModels.Interfaces/Tools/IExecutableToolVM.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace Sudoku.ViewModels.Interfaces.Tools
+{
+    public interface IExecutableToolVM : IToolVM
+    {
+        ICommand ExecuteCommand { get; }
+    }
+}
---
+++ Sudoku.ViewModels/Tools/ExecutableToolVM.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+
+using Sudoku.ViewModels.Framework;
+using Sudoku.ViewModels.Interfaces.Tools;
+
+namespace Sudoku.ViewModels.Tools
+{
+    public abstract class ExecutableToolVM : ToolVM, IExecutableToolVM
+    {
+        private ICommand _executeCommand;
+
+        public ICommand ExecuteCommand
+        {
+            get
+            {
+                return _executeCommand ?? (_executeCommand = new RelayCommand(OnExecute, CanExecute));
+            }
+        }
+
+        protected abstract void OnExecute();
+
+        protected abstract bool CanExecute();
+    }
+}
